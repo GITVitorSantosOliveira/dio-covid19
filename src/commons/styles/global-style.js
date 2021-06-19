759928853608e8ce633baf1be1a0b5522acf00be
Vitor Santos <vitorsantoo134@gmail.com>
2021-06-19 18:39:42 -0300

    completed project

---
+++ src/commons/styles/global-style.js
@@ -0,0 +1,42 @@
+import {createGlobalStyle} from 'styled-components';
+import CovidImg from '../../assets/images/covid.jpg';
+
+const globalStyles = createGlobalStyle `
+    *{
+        outline: none;
+        box-sizing: border-box;
+    }
+
+    body{
+    line-height: normal;
+    }
+
+    html,body{
+        width: 100%;
+        min-height: flex;
+        display: flex;
+        padding: 0;
+        margin: 0;
+    }
+
+    #root{
+        background: url(${CovidImg});
+        height: 100vh;
+        width: 100%;
+        background-size: cover;
+        background-position: center center;
+    }
+
+    .mb-2{
+        margin-bottom: 16px;
+    }
+
+    .pt-2{
+        padding-top: 16px;
+    }
+
+    .cursor{
+        cursor: pointer;
+    }
+`
+export default  globalStyles;
